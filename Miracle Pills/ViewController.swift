a4f1655178391c34ac636c636146f6a98b146ddf
Jose Tapizquent <josetapizq@gmail.com>
2017-04-04 13:13:20 -0400

    First Commit
    All done till now

--- Miracle Pills/ViewController.swift
+++ Miracle Pills/ViewController.swift
@@ -8,11 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    let states = ["Alaska", "Alabama", "Arkansas", "California", "Colorado", "Florida", "Maine", "Ney York", "Kansas"]
+    
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var countryTextField: UITextField!
+    @IBOutlet weak var zipCodeLabel: UILabel!
+    @IBOutlet weak var zipCodeTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        statePicker.dataSource = self
+        statePicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func stateBtnPressed(_ sender: Any) {
+        
+        hideButtons(state: true)
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal)
+        hideButtons(state: false)
+    }
+    
+    func hideButtons(state: Bool){
+        statePicker.isHidden = !state
+        countryLabel.isHidden = state
+        countryTextField.isHidden = state
+        zipCodeLabel.isHidden = state
+        zipCodeTextField.isHidden = state
+    }
 
 }
 
